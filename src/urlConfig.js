5f5e81ad38a441f84a33589bbfc13ececfa4f496
Rosh9532 <roshnichauhan4800@gmail.com>
2021-06-15 00:16:13 +0530

    urlconfig update

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,1 +1,4 @@
-export const api ="http://localhost:2000";
+const baseUrl="https://memories-backend-rest-server.herokuapp.com";
+export const api=`${baseUrl}/api`;
+
+// export const api ="http://localhost:2000";
