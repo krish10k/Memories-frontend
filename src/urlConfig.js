437a5edc084354f10526e02144f1a7d3e68169de
Rosh9532 <roshnichauhan4800@gmail.com>
2021-06-15 00:25:42 +0530

    removed bug

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,4 @@
 const baseUrl="https://memories-backend-rest-server.herokuapp.com";
-export const api=`${baseUrl}/api`;
+export const api=`${baseUrl}`;
 
 // export const api ="http://localhost:2000";
